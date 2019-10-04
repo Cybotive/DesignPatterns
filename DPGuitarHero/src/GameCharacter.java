123b462404c4737b32ddc1bd848c5105100ea20a
Josh Lini <networkmisc@gmail.com>
2019-10-04 16:05:04 -0700

    Fixing Code to fit assignment and UML diagram

--- DPGuitarHero/src/GameCharacter.java
+++ DPGuitarHero/src/GameCharacter.java
@@ -1,56 +1,26 @@
 import java.util.Random;
 
 //Joshua Lini 10-4-19
-public abstract class GameCharacter {
+public class GameCharacter {
 	
-	private String solo;
-	private String guitar;
-	protected String name;//if private I'd need a protected set method which doesn't know what to filter anyway, so pointless
+	private SoloBehavior soloBehavior;
+	private GuitarBehavior guitarBehavior;
+	private String name;
 	
 	public void setGuitar(int sel) {
-		switch(sel) {
-		case 0:
-			this.guitar = "Gibson SG";
-			break;
-		case 1:
-			this.guitar = "Fender Telecaster";
-			break;
-		case 2:
-			this.guitar = "Gibson Flying V";
-			break;
-			
-		default:
-			break;//don't assume default value here
-		}
-	}
-	
-	protected String getGuitar() {
-		return guitar;
+		
 	}
 	
 	public void setSolo(int sel) {
-		switch(sel) {
-		case 0:
-			this.solo = "sets their guitar on fire";
-			break;
-		case 1:
-			this.solo = "jumps off the stage";
-			break;
-		case 2:
-			this.solo = "smashes their guitar";
-			break;
-			
-		default:
-			break;//don't assume default value here
-		}
+		
 	}
 	
-	protected String getSolo() {
-		return solo;
+	public void playGuitar() {
+		
 	}
-	
-	public abstract void playGuitar();
 
-	public abstract void playSolo();
+	public void playSolo() {
+		
+	}
 
 }
