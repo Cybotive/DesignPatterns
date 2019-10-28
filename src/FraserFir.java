23ab190ec882a4b7b86050d7f69b67a127fd2c81
Josh Lini <networkmisc@gmail.com>
2019-10-27 20:12:09 -0700

    Dependency Inversion

--- src/FraserFir.java
+++ src/FraserFir.java
@@ -1,22 +1,8 @@
 //Joshua Lini 10-23-19
 public class FraserFir extends Tree {
 	
-	private double cost;
-	private String name;
-	
-	public FraserFir() {
-		cost = 35.0;
-		name = "Fraser Fir";
-	}
-
-	@Override
-	public double getCost() {
-		return cost;
-	}
-
-	@Override
-	public String getName() {
-		return name;
+	public FraserFir(double cost, String name) {
+		super(35.0, "Fraser Fir");
 	}
 	
 }
--- src/Tree.java
+++ src/Tree.java
@@ -1,10 +1,30 @@
 //Joshua Lini 10-23-19
 public abstract class Tree implements ChristmasItem {
+	
+	private double cost;
+	private String name;
+	
+	public Tree(double cost, String name) {
+		if(cost >= 0.0) {
+			this.cost = cost;
+		}
+		else {
+			throw new IllegalArgumentException("cost must not be negative.");
+		}
+		
+		if(name == null) {
+			throw new IllegalArgumentException("Tree must have a name.");
+		}
+	}
 
 	@Override
-	public abstract double getCost();
+	public double getCost() {
+		return cost;
+	}
 
 	@Override
-	public abstract String getName();
+	public String getName() {
+		return name;
+	}
 	
 }
