4fa83d15a11303a41b86f2304173a5c547616a8b
Josh Lini <networkmisc@gmail.com>
2019-10-29 13:46:00 -0700

    Stubbed Out Test Class

---
+++ src/math_utils/MathFunctions.java
@@ -0,0 +1,11 @@
+package math_utils;
+
+public class MathFunctions {
+	public int sumFrom1ThruN(int n) {
+		return 0;//TODO
+	}
+	
+	public int power(int base, int exp) {
+		return 0;//TODO
+	}
+}
---
+++ src/math_utils/MathFunctionsSumFrom1ThruNTests.java
@@ -0,0 +1,19 @@
+package math_utils;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class MathFunctionsSumFrom1ThruNTests {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@Test
+	void testSumFrom1ThruN() {
+		fail("Not yet implemented");
+	}
+
+}
