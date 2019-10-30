3f9312829aebe73189d61f3a57163ccaa4071d58
Josh Lini <networkmisc@gmail.com>
2019-10-30 13:41:46 -0700

    unit testing function, including expected exception

--- src/math_utils/MathFunctions.java
+++ src/math_utils/MathFunctions.java
@@ -2,7 +2,15 @@ package math_utils;
 
 public class MathFunctions {
 	public int sumFrom1ThruN(int n) {
-		return 0;//TODO
+		if(n < 1) { throw new IllegalArgumentException("n may not be less than 1"); }
+		
+		int sum = 1;
+		
+		for(int i = 2; i <= n; i++) {
+			sum += i;
+		}
+		
+		return sum;
 	}
 	
 	public int power(int base, int exp) {
--- src/math_utils/MathFunctionsSumFrom1ThruNTests.java
+++ src/math_utils/MathFunctionsSumFrom1ThruNTests.java
@@ -6,14 +6,36 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 class MathFunctionsSumFrom1ThruNTests {
-
+	
+	private static MathFunctions mathFunctions;
+	
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
+		mathFunctions = new MathFunctions();
 	}
-
+	
 	@Test
-	void testSumFrom1ThruN() {
-		fail("Not yet implemented");
+	void testSumFrom1ThruN_NIs1() {
+		assertEquals(1, mathFunctions.sumFrom1ThruN(1));
 	}
-
+	
+	@Test
+	void testSumFrom1ThruN_NIs2() {
+		assertEquals(3, mathFunctions.sumFrom1ThruN(2));
+	}
+	
+	@Test
+	void testSumFrom1ThruN_NIs10() {
+		assertEquals(55, mathFunctions.sumFrom1ThruN(10));
+	}
+	
+	@Test
+	void testSumFrom1ThruN_NIs0() {
+		assertThrows(IllegalArgumentException.class, () -> {mathFunctions.sumFrom1ThruN(0);});
+	}
+	
+	@Test
+	void testSumFrom1ThruN_NIsNegative1() {
+		assertThrows(IllegalArgumentException.class, () -> {mathFunctions.sumFrom1ThruN(-1);});
+	}
 }
