129e6a6ae3c218eb06e4218658d4047db9eec19d
Josh Lini <networkmisc@gmail.com>
2019-11-04 13:05:31 -0800

    methods stubbed out

--- JUnitAssignment/src/fraction/Fraction.java
+++ JUnitAssignment/src/fraction/Fraction.java
@@ -4,4 +4,40 @@ package fraction;
 
 public class Fraction {
 
+	public Fraction(int i, int j) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Integer getNum() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Integer getDen() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Integer compareTo(Fraction fraction) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Fraction add(Fraction fraction) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Fraction multiply(Fraction fraction) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public double realValue() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	//TODO Equals and HashCode
+
 }
